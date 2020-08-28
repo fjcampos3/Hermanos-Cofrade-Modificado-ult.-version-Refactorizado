c764127de6a825caed18aa28b7bdb777838c0945
Francisco Jose <franjc92@gmail.com>
2020-08-28 21:27:26 +0200

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import CabeceraMenu from './Header/CabeceraMenu';
+
+const  divStyledisplay  = {
+  display:'inherit',
+  
+};
+
+const  divStyledisplay2  = {
+  display:'inherit',
+  margin: "-90px 0px 0px 40px"
+  
+};
+const  divStylewidth  = {
+  width:'100%',
+
+};
+
+const  divStylewidth2 = {
+  width:'30px',
+};
+
+
+class Header extends React.Component<{}, {}>{
+    public render(){
+        return (<header id="Cabz">
+
+        <div id="cabecera">
+        
+          <div className="logo amplia2">          
+            <a href="http://www.hermanoscofrades.com/" style={divStyledisplay}><img src="./Hermanos Cofrades Sevilla - Ropa y complementos para el cofrade_files/logoPrincipal.png" alt="Hermanos Cofrades 2"></img></a>           
+            <a href="http://www.hermanoscofrades.com/" style={divStyledisplay2}><img src="./Hermanos Cofrades Sevilla - Ropa y complementos para el cofrade_files/logoHnosCofrades.svg" alt="Hermanos Cofrades"></img></a>        
+          </div> 
+          
+          <div id="buscar">            
+            <form id="busca" name="buscar" method="get" action="http://www.hermanoscofrades.com/index.php">
+              <input type="hidden" value="hermanoscofrades" name="p"></input>
+              <input type="hidden" value="1" name="proceed_search"></input>
+              <input type="hidden" value="products" name="mod"></input>
+              <table style={divStylewidth}>
+                <tbody>
+                  <tr>
+                    <td><input placeholder="Buscar" name="search_by" className="buscar efectos"></input></td>
+                    <td className="separador"> </td>
+                    <td style={divStylewidth2}>
+                    <a onClick={(event: React.MouseEvent<HTMLElement>) => {}} href="#" className="botonBuscar ampliar imags"><img src="./Hermanos Cofrades Sevilla - Ropa y complementos para el cofrade_files/busca-lupa.svg" alt="Lupa"></img></a>
+                    </td>
+                  </tr>
+               </tbody>
+              </table>
+            </form>
+            
+          </div>  
+
+
+          <div className="usuarios ampliar">
+          <div className="letraUSCar">Identifícate <br></br> o Regístrate</div>
+            <a href="http://www.hermanoscofrades.com/register/login"><img src="./Hermanos Cofrades Sevilla - Ropa y complementos para el cofrade_files/usuarios.svg" alt="Usuarios"></img></a>
+          </div>
+
+          <div id="carro">
+            <div id="carro-icono" className="ampliar"><div className="letraUSCar">Su carro:</div><img src="./Hermanos Cofrades Sevilla - Ropa y complementos para el cofrade_files/carrito.svg" alt="carrito"></img></div>
+            <div className="carro-flotante">
+              <div className="Cart" id="Cart">
+                <div id="carro-vacio">Su Carrito está vacío</div>
+              </div>
+            </div>
+          </div>
+
+          <CabeceraMenu/>
+
+        </div>
+
+    </header>);
+    }
+}
+
+export default Header;
---
+++ src/components/Header/CabeceraMenu.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const  divStylePadding  = {
+    padding:"0px 5px",
+    
+  };
+
+class CabeceraMenu extends React.Component<{}, {}>{
+    public render(){
+        return (<div id="cabeceraMenu">
+            
+            <ul id="menu">
+              <li><a href="http://www.hermanoscofrades.com/es/173259-inicio">Inicio</a></li>            
+              <li><a href="http://www.hermanoscofrades.com/es/173260-quienes-somos">Quienes Somos</a></li>
+              <li><a href="http://www.hermanoscofrades.com/es/173253-ofertas">Ofertas</a></li>            
+              <li><a href="http://www.hermanoscofrades.com/es/173254-novedades">Novedades</a></li>
+              <li><a href="http://www.hermanoscofrades.com/es/news/18685-noticia-2">Noticias</a></li>                         
+              <li><a href="http://www.hermanoscofrades.com/es/173262-contacto">Contacto</a></li>
+
+              <li><a className= "redesCabecera" style={divStylePadding} href="tel:+34954089932"><img src="./Hermanos Cofrades Sevilla - Ropa y complementos para el cofrade_files/contactoTelf.svg" alt="Telófono"></img></a></li>           
+              <li><a className= "redesCabecera" style={divStylePadding} href="mailto:info@hermanoscofrades.com"><img src="./Hermanos Cofrades Sevilla - Ropa y complementos para el cofrade_files/contactoMail.svg" alt="Email"></img></a></li> 
+              <li><a className= "redesCabecera" style={divStylePadding} href="https://web.whatsapp.com/send?phone=34647858133" target="_blank"><img src="./Hermanos Cofrades Sevilla - Ropa y complementos para el cofrade_files/contactoWhat.svg" alt="WhatsApp"></img></a></li>
+              <li><a className= "redesCabecera" style={divStylePadding} href="https://www.facebook.com/hermanoscofrades" target="_blank"><img src="./Hermanos Cofrades Sevilla - Ropa y complementos para el cofrade_files/socialFacebook.svg" alt="Facebook"></img></a></li>
+              <li><a className= "redesCabecera" style={divStylePadding} href="https://twitter.com/hermanoscofrade" target="_blank"><img src="./Hermanos Cofrades Sevilla - Ropa y complementos para el cofrade_files/socialTwitter.svg" alt="Twitter"></img></a></li>
+              <li><a className= "redesCabecera" style={divStylePadding} href="https://www.instagram.com/hermanoscofradestriana/" target="_blank"><img src="./Hermanos Cofrades Sevilla - Ropa y complementos para el cofrade_files/socialInstagram.svg" alt="Instagram"></img></a></li>
+                     
+            </ul>
+            
+          </div>)
+    }
+}
+
+export default CabeceraMenu;
