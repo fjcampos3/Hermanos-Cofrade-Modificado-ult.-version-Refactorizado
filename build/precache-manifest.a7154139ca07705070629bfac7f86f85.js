64eb94327aa17a186a43cee8822adc91ebab8dc5
fjcampos3 <68522331+fjcampos3@users.noreply.github.com>
2020-12-06 19:49:02 +0100

    Add files via upload

---
+++ build/precache-manifest.a7154139ca07705070629bfac7f86f85.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9085a1b2b8ce5139b8c0242dda6a0de0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1eab81df53dc63057c9f",
+    "url": "/static/css/main.13c8e189.chunk.css"
+  },
+  {
+    "revision": "1992806a648cabd34d5e",
+    "url": "/static/js/2.3fb5b31b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3fb5b31b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eab81df53dc63057c9f",
+    "url": "/static/js/main.40a340a4.chunk.js"
+  },
+  {
+    "revision": "45f156c7f583d4bb7c4d",
+    "url": "/static/js/runtime-main.ba0a0475.js"
+  },
+  {
+    "revision": "bb9c37b805697ba42562eee6c2db9eef",
+    "url": "/static/media/Aleo-B.bb9c37b8.woff"
+  },
+  {
+    "revision": "22fb40bade7001d262f12d479c9be505",
+    "url": "/static/media/Aleo-BI.22fb40ba.woff"
+  },
+  {
+    "revision": "c946c82e467b2ebef332be9a7eb39f4e",
+    "url": "/static/media/Aleo-N.c946c82e.woff"
+  },
+  {
+    "revision": "a73f7e772073a284ded0d423b02fa5d2",
+    "url": "/static/media/Aleo-NI.a73f7e77.woff"
+  },
+  {
+    "revision": "31f29944b8d3f0a95002acd77459944e",
+    "url": "/static/media/cabeceraFondo.31f29944.jpg"
+  },
+  {
+    "revision": "3c8870ed185200d7db016dcc4a80f7e3",
+    "url": "/static/media/paginaFondo.3c8870ed.jpg"
+  },
+  {
+    "revision": "5a88ad4f1af8fed798748944a11606e9",
+    "url": "/static/media/paginaFondoA.5a88ad4f.png"
+  },
+  {
+    "revision": "02fdae75579670330b96dabc237dd70c",
+    "url": "/static/media/paginaFondoB.02fdae75.png"
+  }
+]);
